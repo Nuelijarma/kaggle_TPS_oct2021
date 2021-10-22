430667eeefd2d1ff0b7b113cdb304f4a01db9f6a
Maya <amelie.barbe@ens-lyon.fr>
2021-10-22 18:13:59 +0200

    New solution, based on a MLP built with Keras.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tensorflow import keras
 
 # Classifiers
 from xgboost import XGBClassifier
@@ -19,41 +20,64 @@ logger.setLevel(logging.DEBUG)
 def main():
     logger.debug("Loading training data.")
     data = pd.read_csv("data/train.csv", index_col="id")
-    x = data.drop(["target"], axis=1)
-    y = data[["target"]]
+    train_x = data.drop(["target"], axis=1)
+    train_y = data[["target"]].astype("category")
 
-    logger.debug("Processing training data.")
-    logger.debug("(todo)")
+    logger.debug("Some statistics")
+    n, d = train_x.shape
+    logger.debug(f"- {n} items.")
+    logger.debug(f"- {d} features")
+    nans = train_x.isna().sum().sum()
+    logger.debug(f"- {nans} missing values")
 
-    logger.debug("Train/test split.")
-    train_x, test_x, train_y, test_y = train_test_split(x, y)
+    logger.debug("Processing training data")
+    logger.debug("- One-hot encoding labels")
+    train_y = pd.get_dummies(train_y)
 
-    logger.debug("Training Naive Bayes classifier.")
-    clf = GaussianNB()
-    clf.fit(train_x, train_y.values.ravel())
+    # logger.debug("Train/test split.")
+    # train_x, test_x, train_y, test_y = train_test_split(x, y)
 
-    logger.debug("Testing classifier.")
-    print(f"test score: {roc_auc_score(test_y, clf.predict_proba(test_x)[:,1])}")
+    logger.debug("Training MLP classifier.")
+    clf = keras.Sequential([
+        keras.layers.Dense(d, activation="relu"),
+        keras.layers.Dense(100, activation="relu"),
+        keras.layers.Dense(2, activation="softmax"),
+
+    ])
+    clf.compile(
+        optimizer=keras.optimizers.SGD(),
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=[keras.metrics.BinaryAccuracy(), keras.metrics.AUC()]
+    )
+    clf.fit(
+        x=train_x.values,
+        y=train_y.values,
+        batch_size=256,
+        validation_split=.2,
+        epochs=4,
+        verbose=1)
+
 
     logger.debug("Cleaning")
     del train_x
-    del test_x
     del train_y
-    del test_y
 
     logger.debug("Loading validation data.")
     val_x = pd.read_csv("data/test.csv", index_col="id")
     val_index = val_x.index
 
     logger.debug("Processing validation data.")
-    logger.debug("todo")
+    logger.debug("none")
 
     logger.debug("Predicting test 'target' value.")
-    val_y = clf.predict_proba(val_x)
+    val_y = clf.predict(
+        x=val_x,
+        batch_size=256,
+        verbose=1)
 
     logger.debug("Writing output")
     val_final = pd.DataFrame(data={"id": val_index.values, "target": val_y[:,1]})
-    val_final.to_csv("nb_solution.csv", index=False)
+    val_final.to_csv("mlp_solution.csv", index=False)
 
 if __name__=="__main__":
     main()
